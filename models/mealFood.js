194d81d248ac4458f963c14a9c736db175c5ceaa
JamisonOrdway <jamisonordway@gmail.com>
2018-08-26 12:08:41 -0600

    changes syntax in mealfoods model

--- models/mealFood.js
+++ models/mealFood.js
@@ -2,17 +2,16 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
-class MealFood {
-  static create = (meal_id, food_id) => {
-    return database('meal_foods')
-    .insert({meal_id: meal_id, food_id: food_id})
-    .returning('*')
-  };
+const create = (meal_id, food_id) => {
+  return database('meal_foods')
+  .insert({meal_id: meal_id, food_id: food_id})
+  .returning('*')
+};
 
-  static remove = (meal_id, food_id) => {
-    return database('meal_foods')
-    .where({meal_id: meal_id, food_id: food_id})
-    .del()
-  }
+const remove = (meal_id, food_id) => {
+  return database('meal_foods')
+  .where({meal_id: meal_id, food_id: food_id})
+  .del()
 }
-module.exports = MealFood
+
+module.exports = {create, remove}
