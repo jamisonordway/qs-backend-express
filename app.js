0e6025d2ab34dadb5d62ebf2116b16c087350b0f
JamisonOrdway <jamisonordway@gmail.com>
2018-08-26 11:55:49 -0600

    Adds different allow access control origin

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const foodsRouter = require('./routes/api/v1/foods')
 const mealsRouter = require('./routes/api/v1/meals')
 
@@ -25,7 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(allowCrossDomain)
 app.use(cors())
 app.options('*', cors())
 
