101c83de7bc8819422797022a53336ea4602d3cd
JamisonOrdway <jamisonordway@gmail.com>
2018-08-26 11:52:14 -0600

    adds allowCrossDomain

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(allowCrossDomain)
 app.use(cors())
 app.options('*', cors())
 
